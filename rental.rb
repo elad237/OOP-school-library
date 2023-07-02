92a04c42eea39659783dfbb4b6bc096524b43cf9
TUMBU BARTHOLOMEW ELAD <tumbu.elad@gmail.com>
2023-07-01 19:01:33 -0700

    Upadted the rental.rb file

--- rental.rb
+++ rental.rb
@@ -1,37 +1,24 @@
 class Rental
   attr_accessor :date, :book, :person
 
+  @rentals = [] # Class instance variable to store all rentals
+
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
+
+    @book.rentals << self
+    @person.rentals << self
+    self.class.rentals << self
+  end
+
+  class << self
+    attr_reader :rentals
+  end
+
+  def self.rentals_for_person(person_id)
+    rentals.select { |rental| rental.person.id == person_id }
   end
 end
 
-class Book
-  attr_accessor :title, :rentals
-
-  def initialize(title)
-    @title = title
-    @rentals = []
-  end
-
-  def add_rental(person, date)
-    rental = Rental.new(date, self, person)
-    @rentals << rental
-  end
-end
-
-class Person
-  attr_accessor :name, :rentals
-
-  def initialize(name)
-    @name = name
-    @rentals = []
-  end
-
-  def add_rental(book, date)
-    rental = Rental.new(date, book, self)
-    @rentals << rental
-  end
-end
